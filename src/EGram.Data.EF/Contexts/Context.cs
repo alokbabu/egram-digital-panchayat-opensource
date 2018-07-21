17c733e681c88e53daf2a5fcb6c08da28a6854a0
alokbabu <alokbabu@nextuz.com>
2018-07-21 19:17:08 +0100

    * Context.cs: - DbContext updates
    * Survey.cs:
    * EGram.Data.EF.csproj:
    * SurveyEntityTypeConfiguration.cs:
    * SurveyEntityTypeConfiguration.cs:

--- src/EGram.Data.EF/Contexts/Context.cs
+++ src/EGram.Data.EF/Contexts/Context.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using EGram.Data.EF.EntityConfiguration;
+using EGram.Data.EF.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace EGram.Data.EF.Contexts
 {
-    public class Context
+    public class Context: DbContext
     {
-        public Context()
+        public Context(DbContextOptions<Context> options)
+            :base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new SurveyEntityTypeConfiguration());
+        }
+
+        public DbSet<Survey> Surveys { get; set;}
+
     }
 }
--- src/EGram.Data.EF/Models/Survey.cs
+++ src/EGram.Data.EF/Models/Survey.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EGram.Data.EF.Models
 {
     public class Survey
     {
-        public Survey()
-        {
-        }
+        [Key]
+        public int SurveyId { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public int YearId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
     }
 }
